00c251a71f82a7defb2311903ba0e44f6ebcbef0
got-spot <137681097+got-spot@users.noreply.github.com>
2023-09-18 22:41:02 +0900

    Update 6097.py

--- 6097.py
+++ 6097.py
@@ -4,10 +4,10 @@ grid=[[0 for j in range(w)] for i in range(h)]
 n=int(input())
 for i in range(n) :
     l,d,x,y=map(int,input().split())
-    x = x-1
+    x = x-1 #(1,1)이 리스트에선 (2,2)이므로 1씩 빼줌
     y = y-1
     for j in range(l):
-        if d == False:
+        if d == False: # 가로
             grid[x][y+j] = 1
         else:
             grid[x+j][y] = 1
